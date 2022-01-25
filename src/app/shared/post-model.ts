bf05176e5855b18c28e460568e490b45f5bbbe33
Guillaume Muller <guimulxxvi@live.fr>
2022-01-25 18:36:38 +0100

    initial commit

---
+++ src/app/shared/post-model.ts
@@ -0,0 +1,13 @@
+export class PostModel {
+    id: number=0;
+    postName: string ="";
+    url: string="";
+    description: string="";
+    voteCount: number=0;
+    userName: string="";
+    subredditName: string="";
+    commentCount: number=0;
+    duration: string="";
+    upVote: boolean = false;
+    downVote: boolean = false;
+}
